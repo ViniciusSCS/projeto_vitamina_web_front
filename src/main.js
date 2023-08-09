129d2bdcfa1c3cd5e6fb36a8e8bf6e57dd031802
Vinícius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2023-08-08 22:47:59 -0300

    Adicionando bibliotecas

--- src/main.js
+++ src/main.js
@@ -6,7 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import * as Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.prototype.$http = axios
+Vue.prototype.$urlApi = 'http://localhost:8000/api/'
+
 const app = createApp(App)
+
+app.use(VueAxios, axios)
 
 app.use(createPinia())
 app.use(router)
